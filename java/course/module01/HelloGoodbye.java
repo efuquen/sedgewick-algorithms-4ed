dfabf09d6ef105a736dfb5c1038306cca0c95def
Edwin Fuquen <efuquen@gmail.com>
2026-04-18 17:44:45 -0600

    Java module 1 hello assignment.

---
+++ java/course/module01/HelloGoodbye.java
@@ -0,0 +1,13 @@
+public class HelloGoodbye {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println(
+                    "Require exactly two names, not: " + args.length);
+            return;
+        }
+        System.out.println("Hello " + args[0] + " and " + args[1]);
+        System.out.println("Goodbye " + args[1] + " and " + args[0]);
+    }
+
+}
---
+++ java/course/module01/RandomWord.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Optional;
+
+class RandomWord {
+    public static void main(String[] args) {
+        Optional<String> champion = Optional.empty();
+        int i = 1;
+        while (!StdIn.isEmpty()) {
+            String currentWord = StdIn.readString();
+            if (!champion.isPresent() || StdRandom.bernoulli(1.0/i)) {
+                System.out.println("currentWord=" + currentWOrd);
+                champion = Optional.of(currentWord);
+            }
+            i += 1;
+        }
+        System.out.println(champion.get());
+    }
+}
